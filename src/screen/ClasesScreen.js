8b398f0b6e247fdbd7c981a8ad5a1c24537e72d5
Andres Stiven <estibenmaoli@gmail.com>
2026-09-15 16:18:59 -0500

    Screen Clases, aplicar los scrollviews y busquedas

--- src/screen/ClasesScreen.js
+++ src/screen/ClasesScreen.js
@@ -1,32 +1,57 @@
-import React, {useState} from 'react';
-import { View, Text, TextInput,FlatList, ScrollView,  StyleSheet } from 'react-native';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import {Ionicons} from '@expo/vector-icons';
+import React, { useState } from 'react';
+import { View, Text, TextInput, FlatList, ScrollView, StyleSheet } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Ionicons } from '@expo/vector-icons';
 
 import Card from '../components/Card';
+import NivelFiltro from '../components/NivelFiltro';
 import { colors, spacing, typography } from '../theme';
-import {CLASES, NIVELES} from '../data/clases';
-import { useState } from 'react';
+import { CLASES, NIVELES } from '../data/clases';
 
-export default function ClasesScreen({navigation}) {
+
+export default function ClasesScreen({ navigation }) {
     // const {columnas, paddingHorizontal} = useReponsive();
-    const [nivel, setNivel] = useState('')
+    const [nivel, setNivel] = useState('Todos');
+    const [busqueda, setBusqueda] = useState('');
 
     return (
         <View>
             <View>
                 <Text> Aplicacion de clases de ingles </Text>
                 <View>
-                    <Ionicons name="search" size={18}/>
-                    <TextInput 
-                    placeholder= 'Buscar por nivel o profesor'
-                    value={nivel}
-                    onChangeText={setNivel}
-                    autoCorrect={False}
+                    <Ionicons name="search" size={18} />
+                    <TextInput
+                        placeholder='Buscar por nivel o profesor'
+                        value={busqueda}
+                        onChangeText={setBusqueda}
+                        autoCorrect={false}
+                        autoComplete="off"
                     />
-                </View> //debajo de este colocar card y luego el flylist
+
+                    {busqueda.length > 0 && (
+                        <Ionicons
+                            name="close-circle"
+                            size={18}
+                            onPress={() => setBusqueda('')}
+                        />
+                    )}
+                </View>
+                {/** debajo de este colocar card y luego el flylist */}
+                <ScrollView
+                    style={{ flexGrow: 0 }}
+                >
+                    {/** repasar el metodo .map de js */}
+                    <ScrollView horizontal style={{ flexGrow: 0 }}>
+                        {NIVELES.map((item) => (
+                            <NivelFiltro
+                                etiqueta={item}
+                                activo={nivel === item}
+                                onPress={() => setNivel(item)}
+                            />
+                        ))}
+                    </ScrollView>
+                </ScrollView>
             </View>
         </View>
-
     )
 }
